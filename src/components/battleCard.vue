835ee0af7514de3569f74d036a0e77e2da487af2
jack <jack@jacks-MacBook-Pro.local>
2018-02-05 01:12:16 +0000

    Factor out the battlecard into its own component

---
+++ src/components/battleCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="col-sm-4">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title" v-on:click="shoutOut"> {{ battle.name }} </h3>
+      </div>
+      <div class="panel-body" v-on:click="shoutOut">
+        <p><span class="badge alert-info"> Sponsor: </span> {{ battle.sponsor }} </p>
+        <p><span class="badge alert-danger"> SeedFund: </span><strong> ${{ battle.seedFund }} </strong></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'battle-card',
+  props: ['battle'],
+  data() {
+    return {
+      name: '',
+      sponsor: '',
+      seedFund: '',
+    };
+  },
+  methods: {
+    shoutOut() {
+      console.log(this.battle.name);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/publicBattles.vue
+++ src/components/publicBattles.vue
@@ -2,31 +2,23 @@
   <div>
     <app-nav></app-nav>
     <h3 class="text-center">Daily Startup Battles</h3>
-    <button class="btn btn-info log" @click="updateBattles()">Update Battles</button>
+    <button class="btn btn-info log" v-on:click="updateBattles()">Update Battles</button>
     <hr/>
-    <div class="col-sm-4" v-for="battle in publicBattles">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title"> {{ battle.name }} </h3>
-        </div>
-        <div class="panel-body">
-          <p><span class="badge alert-info"> Sponsor: </span> {{ battle.sponsor }} </p>
-          <p><span class="badge alert-danger"> SeedFund: </span><strong> ${{ battle.seedFund }} </strong></p>
-        </div>
-      </div>
-    </div>
+    <battle-card v-for="battle in publicBattles" v-bind:battle="battle" v-bind:key="battle.id"></battle-card>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import AppNav from './AppNav';
+import BattleCard from './battleCard';
 import { getPublicStartupBattles } from '../../utils/battles-api';
 
 export default {
   name: 'publicBattles',
   components: {
     AppNav,
+    BattleCard,
   },
   data() {
     return {
